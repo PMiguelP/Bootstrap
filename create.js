d3aa0b1020bbb49bbc399599ac0c0a82d9f8c196
Miguel Pereira <miguel@Miguels-MacBook-Air.local>
2024-05-09 09:44:40 +0100

    funcoes para pegar tudo na pagina

--- create.js
+++ create.js
@@ -1,34 +1,32 @@
-// aqui vou criar tudo o que peguei dos botoes 
+// aqui vou criar tudo o que peguei dos botoes
 
-botaoRegistrarAluno.addEventListener("click", function(event) {
-    event.preventDefault();
-    let aluno = obtemAlunoDoFormulario(formularioAlunos);
-    alert(aluno.nif);
-    alert(aluno.telemovel);
-    montaTr(aluno);
-    formularioAlunos.reset();
-})
-
+botaoRegistrarAluno.addEventListener("click", function (event) {
+  event.preventDefault();
+  let aluno = obtemAlunoDoFormulario(formularioAlunos);
+  alert(aluno.nif);
+  alert(aluno.telemovel);
+  montaTr(aluno);
+  formularioAlunos.reset();
+});
 
 function obtemAlunoDoFormulario(formularioAlunos) {
-    let aluno = {
-        nome: formularioAlunos.name.value,
-        nif: formularioAlunos.nif.value,
-        telemovel: formularioAlunos.telemovel.value
-    }
-    return aluno;
+  let aluno = {
+    nome: formularioAlunos.name.value,
+    nif: formularioAlunos.nif.value,
+    telemovel: formularioAlunos.telemovel.value,
+  };
+  return aluno;
 }
 
 function montaTr(aluno) {
-    let alunoTr = document.createElement("Tr");
-    alunoTr.classList.add("aluno");
+  let alunoTr = document.createElement("Tr");
+  alunoTr.classList.add("aluno");
 
-        let alunoTh = document.createElement("th");
-        alunoTh.classList.add("name");
-        alunoTh.setAttribute("scope", "row");
+  let alunoTh = document.createElement("th");
+  alunoTh.classList.add("name");
+  alunoTh.setAttribute("scope", "row");
 
-        let niftd = montaTd("nif", aluno.nif);
-        let telemovel = montaTd("tel", aluno.telemovel);
-        let 
-
+  let niftd = montaTd("nif", aluno.nif);
+  let telemovel = montaTd("tel", aluno.telemovel);
+  let;
 }
